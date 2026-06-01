0ab1adf190d64a50abfc68a5779b1cf212434e4b
Saif Hasan Saoum <saifsaoum0506@gmail.com>
2026-06-02 00:39:41 +0600

    Adjust motor speeds and implement turn memory
    Updated motor speeds for better performance and added last turn memory functionality.

--- Arduino_code/last_code/last_code.ino
+++ Arduino_code/last_code/last_code.ino
@@ -8,15 +8,19 @@
 #define IN4 11
 
 // ================= SPEED =================
-int baseLeft  = 98;
-int baseRight = 100;
-int turnSpeed = 105;
+int baseLeft  = 255;   // FULL SPEED
+int baseRight = 255;   // FULL SPEED
+int turnSpeed = 220;   // Slightly less for control
 
 // ================= SENSOR PINS =================
 int sensors[5] = {A5, A4, A3, A2, A1};
 
 // ================= BLACK THRESHOLD =================
 int BLACK = 200;
+
+// ================= LAST TURN MEMORY =================
+enum LastTurn {NONE, LEFT, RIGHT};
+LastTurn lastTurn = NONE;
 
 void setup() {
   pinMode(ENA, OUTPUT);
@@ -32,37 +36,40 @@ void setup() {
 
 // ================= MOTOR FUNCTIONS =================
 void goStraight() {
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN4, LOW);
   analogWrite(ENA, baseLeft);
   analogWrite(ENB, baseRight);
 }
 
 void turnLeft() {
-  // LEFT motor STOP
-  analogWrite(ENA, 0);
-  // RIGHT motor FORWARD
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);             // Left motor stop
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);           // Right motor forward
   analogWrite(ENB, turnSpeed);
+  lastTurn = LEFT;
 }
 
 void turnRight() {
-  // RIGHT motor STOP
-  analogWrite(ENB, 0);
-  // LEFT motor FORWARD
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  analogWrite(ENB, 0);             // Right motor stop
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);           // Left motor forward
   analogWrite(ENA, turnSpeed);
+  lastTurn = RIGHT;
 }
 
 // ================= MAIN LOOP =================
 void loop() {
-  int s5 = analogRead(A5);
-  int s4 = analogRead(A4);
-  int s3 = analogRead(A3);
-  int s2 = analogRead(A2);
-  int s1 = analogRead(A1);
 
-  // Debug
+  int s5 = analogRead(A5);  // Far Left
+  int s4 = analogRead(A4);  // Left
+  int s3 = analogRead(A3);  // Center
+  int s2 = analogRead(A2);  // Right
+  int s1 = analogRead(A1);  // Far Right
+
+  // ===== DEBUG =====
   Serial.print(s5); Serial.print("\t");
   Serial.print(s4); Serial.print("\t");
   Serial.print(s3); Serial.print("\t");
@@ -70,18 +77,63 @@ void loop() {
   Serial.println(s1);
 
   // ===== LOGIC =====
-  if (s5 < BLACK || s4 < BLACK) {
-    turnLeft();
-  }
-  else if (s2 < BLACK || s1 < BLACK) {
-    turnRight();
-  }
-  else if (s3 < BLACK) {
-    goStraight();
-  }
-  else {
-    goStraight();
+
+  // VERY HARD LEFT (90° TURN)
+  if (s5 < BLACK && s4 < BLACK) {
+    analogWrite(ENA, 0);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, 255);
+    lastTurn = LEFT;
   }
 
-  delay(5);
+  // VERY HARD RIGHT (90° TURN)
+  else if (s1 < BLACK && s2 < BLACK) {
+    analogWrite(ENB, 0);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, 255);
+    lastTurn = RIGHT;
+  }
+
+  // HARD LEFT
+  else if (s5 < BLACK) {
+    turnLeft();
+  }
+
+  // NORMAL LEFT
+  else if (s4 < BLACK) {
+    turnLeft();
+  }
+
+  // HARD RIGHT
+  else if (s1 < BLACK) {
+    turnRight();
+  }
+
+  // NORMAL RIGHT
+  else if (s2 < BLACK) {
+    turnRight();
+  }
+
+  // STRAIGHT
+  else if (s3 < BLACK) {
+    goStraight();
+    lastTurn = NONE;
+  }
+
+  // ALL WHITE → SEARCH LAST DIRECTION
+  else {
+    if (lastTurn == LEFT) {
+      turnLeft();
+    } 
+    else if (lastTurn == RIGHT) {
+      turnRight();
+    } 
+    else {
+      goStraight();
+    }
+  }
+
+  delay(1);  // Faster response
 }
